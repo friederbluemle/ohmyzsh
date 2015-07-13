13f1782c455d6a960b9d8859d5fe4ba580fb5fdc
Frieder Bluemle <frieder.bluemle@gmail.com>
2015-07-13 15:16:58 +0800

    Improve up-down command search

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -106,3 +106,12 @@ if [ ! "$ZSH_THEME" = ""  ]; then
     source "$ZSH/themes/$ZSH_THEME.zsh-theme"
   fi
 fi
+
+autoload -Uz up-line-or-beginning-search
+autoload -Uz down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey '\eOA' up-line-or-beginning-search
+bindkey '\e[A' up-line-or-beginning-search
+bindkey '\eOB' down-line-or-beginning-search
+bindkey '\e[B' down-line-or-beginning-search
