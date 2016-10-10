38036f14caa3dd2b57a44b90a3a4895442e71944
Frieder Bluemle <frieder.bluemle@gmail.com>
2016-10-10 11:10:02 +0800

    Revert "Improve up-down command search"
    This reverts commit b01e802d04e10a2a183047b2219fff1e88bf62ca.

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -185,12 +185,3 @@ if [[ -n "$ZSH_THEME" ]]; then
     echo "[oh-my-zsh] theme '$ZSH_THEME' not found"
   fi
 fi
-
-autoload -Uz up-line-or-beginning-search
-autoload -Uz down-line-or-beginning-search
-zle -N up-line-or-beginning-search
-zle -N down-line-or-beginning-search
-bindkey '\eOA' up-line-or-beginning-search
-bindkey '\e[A' up-line-or-beginning-search
-bindkey '\eOB' down-line-or-beginning-search
-bindkey '\e[B' down-line-or-beginning-search
