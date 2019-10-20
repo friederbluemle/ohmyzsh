2112f56fa313c8101acc7836ddc002deae9814b0
Frieder Bluemle <frieder.bluemle@gmail.com>
2019-10-20 11:32:10 -0700

    Update grepos

--- custom/functions.zsh
+++ custom/functions.zsh
@@ -151,7 +151,7 @@ gcl() {
 
 # git list repos under org
 grepos() {
-  hub api "orgs/${1:-fbluemle}/repos?per_page=100" | jq '.[].clone_url'
+  hub api "orgs/${1:-fbluemle}/repos?per_page=100" | jq ".[].clone_url"
 }
 
 # git pull request checkout
